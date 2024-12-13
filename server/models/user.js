12537165b7adff678baf331754f77373a1c0ab40
Dagna Schmidt <dagdagi889@gmail.com>
2024-12-13 14:48:53 +0100

    feat: login, register user backend added
    added User model
    added login controller
    added register controller
    added authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,37 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    // user model
+    username: {
+        type: String,
+        required: true,
+        minLength: 3,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function (v) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email!`
+        }
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: Array,
+        required: false
+        // update later this part to time_id[]
+    },
+    tasks: {
+        type: Array,
+        required: false
+        // update later this part to task_id[]
+    }
 });
 
 userSchema.set('toJSON', {
