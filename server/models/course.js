4f166650e0b6ab1285a8a338418da2ea586f9ac3
Faustina-Acheampong <157317178+Faustina-Acheampong@users.noreply.github.com>
2024-12-15 15:57:24 +0100

    initial commit

--- server/models/course.js
+++ server/models/course.js
@@ -1,7 +1,87 @@
 import mongoose from 'mongoose';
 
+// Course schema
 const courseSchema = new mongoose.Schema({
-    // course model
+    course_name: {
+        type: String,
+        required: [true, 'Course name is required'],
+        maxlength: [250, 'Course name should not exceed 250 characters'],
+        trim: true
+    },
+    course_day: {
+        type: String,
+        enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+        required: [true, 'Course day is required']
+    },
+    course_code: {
+        type: String,
+        required: true,
+        uppercase: true,
+        validate: {
+            validator: function (value) {
+                return /^[A-Z]{2}[0-9]{4}$/.test(value);
+            },
+            message: 'Course code must be in the format: AB-1234'
+        }
+    },
+    course_instructor: {
+        type: String,
+        required: [true, 'Course instructor is required'],
+        maxlength: [250, 'Course instructor should not exceed 250 characters'],
+        trim: true
+    },
+    course_semester: {
+        type: String,
+        required: [true, 'Course semester is required'],
+        validate: {
+            validator: function (value) {
+                return /^(Autumn|Winter|Spring|Summer)-([1-9]|10)$/.test(value);
+            },
+            message: 'Semester must be in the format: Fall-1'
+        }
+    },
+    course_location: {
+        type: String,
+        maxlength: [250, 'Course location should not exceed 250 characters'],
+        trim: true
+    },
+    course_cover_image: {
+        type: String,
+        maxlength: 250
+    },
+    course_description: {
+        type: String,
+        trim: true
+    },
+    course_time: {
+        start: {
+            type: String,
+            required: true,
+            validate: {
+                validator: function(v) {
+                    return /^([01]\d|2[0-3]):([0-5]\d)$/.test(v);
+                },
+                message: 'Invalid time format. Use HH:MM (24-hour format)'
+            }
+        },
+        end: {
+            type: String,
+            required: true,
+            validate: {
+                validator: function(v) {
+                    return /^([01]\d|2[0-3]):([0-5]\d)$/.test(v);
+                },
+                message: 'Invalid time format. Use HH:MM (24-hour format)'
+            }
+        }
+    },
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'User ID is required']
+    },
+}, { 
+    timestamps: true  // To add createdAt and updatedAt automatically
 });
 
 courseSchema.set('toJSON', {
@@ -13,4 +93,5 @@ courseSchema.set('toJSON', {
 });
 
 const Course = mongoose.model('Course', courseSchema);
+
 export default Course;
