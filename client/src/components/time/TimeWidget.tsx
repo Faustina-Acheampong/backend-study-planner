b86d9af9d632c9d34cd0d0abcace0ac713f2bb2f
Tasneemhussain <tasneemhussain321@gmail.com>
2024-12-16 20:35:50 +0100

    added stop timer

--- client/src/components/time/TimeWidget.tsx
+++ client/src/components/time/TimeWidget.tsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import { useState, useEffect, useRef } from 'react';
+import React from "react";
+import { useState, useEffect, useRef } from "react";
 
 const TimeWidget = () => {
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef<NodeJS.Timeout | number | null>(null);
 
-    const[time, setTime] = useState(0);
-    const[isRunning, setIsRunning]= useState(false);
-    const intervalRef = useRef<NodeJS.Timeout | number | null>(null);
+  // Start the timer function
+  const startTime = () => {
+    if (!isRunning) {
+      intervalRef.current = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+      setIsRunning(true);
+    }
+  };
 
-     // Start the timer function
-    const startTime = () =>{
-        if (!isRunning) {
-            intervalRef.current = setInterval (() => {
-              setTime((prevTime) => prevTime + 1);
-            },1000);
-            setIsRunning(true);
-        }
-    };
-    return (
-        <div>
-            TimeWidget
-            {/* component that is in the menu */}
-            {/* display time */}
-            
-            {/* stop, start counting */}
-        </div>
-    );
+  // Stop the timer function
+  const stopTimer = () => {
+    if (!isRunning) {
+      clearInterval(intervalRef.current as NodeJS.Timeout | number);
+      intervalRef.current = null;
+      setIsRunning(false);
+    }
+  };
+
+  return (
+    <div>
+      TimeWidget
+      {/* component that is in the menu */}
+      {/* display time */}
+      {/* stop, start counting */}
+    </div>
+  );
 };
 
 export default TimeWidget;
