41ba3c7392ec083794d92510917db2e097a8b96e
Tasneemhussain <tasneemhussain321@gmail.com>
2024-12-17 19:41:29 +0100

    intial commit

--- server/controllers/tasks.js
+++ server/controllers/tasks.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import mongoose from'mongoose';
 import Task from '../models/task.js';
+import User from '../models/user.js';
+
 
 export const tasksRouter = express.Router();
+
 
 tasksRouter.post('/', (req, res) => {
 
 });
+
+
+tasksRouter.get('/', (req, res) => {
+    res.json("Hello, tasks!");
+  });
--- server/models/task.js
+++ server/models/task.js
@@ -1,16 +1,35 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
-    // task model
+  // task model
+  title: { type: String, required: true },
+  description: String,
+  deadline: Date,
+  status: {
+    type: String,
+    enum: ["pending", "completed"],
+    default: "pending",
+  },
+  priority: {
+    type: String,
+    enum: ["low", "medium", "high"],
+    default: "medium",
+  },
+  completed: { type: Boolean, default: false },
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
-taskSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString();
-        delete returnedObject._id;
-        delete returnedObject.__v;
-    }
+taskSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
 });
 
-const Task = mongoose.model('Task', taskSchema);
+const Task = mongoose.model("Task", taskSchema);
 export default Task;
