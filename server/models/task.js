ad8512d3434a55705035e746c5354ceb70f38cd8
Tasneemhussain <tasneemhussain321@gmail.com>
2024-12-18 18:09:56 +0100

    final commit

--- server/models/task.js
+++ server/models/task.js
@@ -1,26 +1,27 @@
 import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
-  // task model
-  title: { type: String, required: true },
-  description: String,
-  deadline: Date,
-  status: {
-    type: String,
-    enum: ["pending", "completed"],
-    default: "pending",
-  },
-  priority: {
-    type: String,
-    enum: ["low", "medium", "high"],
-    default: "medium",
-  },
-  completed: { type: Boolean, default: false },
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
+
+ // task model
+ taskTitle: { type: String, required: true },
+ taskDescription: { type: String },
+ taskDueDate: { type: Date },
+ taskStatus: {
+   type: String,
+   enum: ['pending', 'completed'],
+   default: 'pending',
+ },
+ taskPriority: {
+   type: String,
+   enum: ['Low', 'Medium', 'High'],
+   default: 'Medium',
+ },
+ isTaskCompleted: { type: Boolean, default: false },
+ userId: {
+   type: mongoose.Schema.Types.ObjectId,
+   ref: 'User',
+   required: true,
+ },
 });
 
 taskSchema.set("toJSON", {
