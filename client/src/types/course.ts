9f85354c9417ecf7e4e4f0fe22263c9a5c7289ae
Faustina-Acheampong <157317178+Faustina-Acheampong@users.noreply.github.com>
2024-12-23 00:36:10 +0100

    Update CourseType with more properties from model

--- client/src/types/course.ts
+++ client/src/types/course.ts
@@ -1,18 +1,22 @@
 import { NoteType } from "./note";
+import { AssignmentType } from "./assignment";
 
 type Days = 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday';
-type Semester = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+type Status = 'Ongoing' | 'Upcoming' | 'Completed' | 'Archived';
 
 export type CourseType = {
     course_name: string,
     course_day: Days,
     course_code: string,
     course_instructor: string,
-    course_semester: Semester,
+    course_semester: string,
     course_location: string,
     course_cover_image: string,
     course_description: string,
-    course_time: number,
+    course_time: string,
     user_id: number,
-    notes?: NoteType[]
+    course_status: Status,
+    is_archived: boolean,
+    notes?: NoteType[],
+    assignments?: AssignmentType[]
 };
