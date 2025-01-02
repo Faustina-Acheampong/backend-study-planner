8a6b26dff820c01a198ac6287850f9cf3313f397
Tasneemhussain <tasneemhussain321@gmail.com>
2025-01-02 19:46:11 +0100

    added modals states

--- client/src/components/schedule/TaskComponent.tsx
+++ client/src/components/schedule/TaskComponent.tsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const TaskComponent = () => {
+
+  const [tasks, setTasks] = useState([]);
+  const [isAddModalOpen, setAddModalOpen] = useState(false);
+  const [isEditModalOpen, setEditModalOpen] = useState(false); 
+  const [formData, setFormData] = useState({
+    task_title: "",
+    task_description: "",
+    task_priority: "Medium",
+    task_due_date: "",
+    assignment_id: "", // Optional field for assignment association
+  });
+  const [editTaskId, setEditTaskId] = useState(null); 
+  
   return (
     <div className="container">
         <h1>Allah</h1>
